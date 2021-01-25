ba9a9c984df772016548b5ae64f2b40d16456225
cheubes <christophe@heubes.com>
2021-01-25 11:57:31 +0100

    Pass test_deposit.

--- budget.py
+++ budget.py
@@ -1,7 +1,14 @@
 class Category:
+    category = ''
+    ledger = []
 
+    def __init__(self, category):
+        self.category = category
 
+    def deposit(self, amount, description):
+        self.ledger.append({'amount':amount, 'description':description})
 
 
 
 def create_spend_chart(categories):
+    return ''
