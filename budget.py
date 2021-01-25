4edb6e41814cecad2f03750a1877be8e39446744
cheubes <christophe@heubes.com>
2021-01-25 14:49:45 +0100

    Pass test_create_spend_chart.

--- budget.py
+++ budget.py
@@ -1,9 +1,12 @@
+import math
+
 class Category:
 
     def __init__(self, category):
         self.category = category
         self.ledger = []
         self.balance = 0
+        self.spent = 0
 
     def deposit(self, amount, description = ''):
         self.ledger.append({'amount':amount, 'description':description})
@@ -14,6 +17,7 @@ class Category:
         if self.check_funds(amount) :
             self.ledger.append({'amount':-amount, 'description':description})
             self.balance -= amount
+            self.spent += amount
             return True
         else :
             return False
@@ -48,4 +52,44 @@ class Category:
 
 
 def create_spend_chart(categories):
-    return ''
+    total_spent = 0
+    for category in categories :
+        total_spent += category.spent
+    category_names = []
+    percentages_spent = []
+    for category in categories :
+        category_names.append(category.category)
+        percentages_spent.append(math.floor(category.spent/total_spent*10)*10)
+    print(percentages_spent)
+
+    spend_chart = 'Percentage spent by category\n'
+
+    for i in range(100, -1, -10) :
+        spend_chart += f'{i:3}|'
+        for p in percentages_spent :
+            if p >= i :
+                spend_chart +=  ' o '
+            else :
+                spend_chart +=  '   '
+        spend_chart += ' \n'
+    spend_chart += '    '
+    for p in percentages_spent :
+        spend_chart += '---'
+    spend_chart += '-\n'
+
+    max_len = 0
+    for name in category_names :
+        if len(name) > max_len :
+            max_len = len(name)
+    for i in range(0, max_len) :
+        spend_chart += '    '
+        for name in category_names :
+            if i < len(name) :
+                spend_chart += f' {name[i]} '
+            else :
+                spend_chart += '   '
+        spend_chart += ' '
+        if i != max_len - 1 :
+            spend_chart += '\n'
+
+    return spend_chart
