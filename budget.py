6d7da11527d2cd1171b67031b90f86f5e01ca9c1
cheubes <christophe@heubes.com>
2021-01-25 12:09:15 +0100

    Pass test_get_balance.

--- budget.py
+++ budget.py
@@ -3,15 +3,20 @@ class Category:
     def __init__(self, category):
         self.category = category
         self.ledger = []
+        self.balance = 0
 
     def deposit(self, amount, description = ''):
         self.ledger.append({'amount':amount, 'description':description})
+        self.balance += amount
         return True
 
     def withdraw(self, amount, description = ''):
         self.ledger.append({'amount':-amount, 'description':description})
+        self.balance -= amount
         return True
 
+    def get_balance(self):
+        return self.balance
 
 def create_spend_chart(categories):
     return ''
